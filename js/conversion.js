b1f19bbdb42ca4e74d104026437f7c218ad2b6dc
Daniel Egele <danielegele9@gmail.com>
2023-11-16 11:27:14 +0100

    all added files

--- js/conversion.js
+++ js/conversion.js
@@ -1,2 +1,26 @@
-const man = "the man God Uses";
-console.log(man.split(""));
+// const man = "the-stealth-warrio";
+// function  upperKase(str){
+//     return str.replace(/-/g, "");
+// }
+// console.log(upperKase("the-stealth-warrio"));
+
+// // convert this string to an array and make the firdt letter uppercase
+// let string = "the-stealth-warrio";
+
+// const wordCase = (str)=> {
+//     const arr = str.split("");
+//     arr.replace(/-/g, "");
+//     arr.charAt(0).toupperCase()+arr.slice(1);
+//     str = arr.join("");
+//     console.log(str);
+
+// }
+// wordCase("the-stealth-warrio");
+
+const appWord = (str) =>{
+    const arr = str.split("");
+    arr.reverse();
+    console.log(arr);
+
+}
+appWord("apple");
